fc01a7f9e1de801414fbfc63dd8f2a66107ddf07
sk89q <the.sk89q@gmail.com>
2015-06-20 22:16:12 -0700

    Add EnumProvider.

---
+++ intake/src/main/java/com/sk89q/intake/parametric/provider/EnumProvider.java
@@ -0,0 +1,87 @@
+/*
+ * Intake, a command processing library
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) Intake team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.intake.parametric.provider;
+
+import com.sk89q.intake.argument.ArgumentException;
+import com.sk89q.intake.argument.ArgumentParseException;
+import com.sk89q.intake.argument.CommandArgs;
+import com.sk89q.intake.parametric.Provider;
+import com.sk89q.intake.parametric.ProvisionException;
+
+import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Searches an enum for a near-matching value.
+ *
+ * <p>When comparing for a match, both the search and test entry have
+ * non-alphanumeric characters stripped.</p>
+ *
+ * @param <T> The type of the enum
+ */
+public class EnumProvider<T extends Enum<T>> implements Provider<T> {
+
+    private static final Pattern NON_ALPHANUMERIC = Pattern.compile("[^A-Za-z0-9]");
+
+    private final Class<T> enumClass;
+
+    /**
+     * Create a new instance.
+     *
+     * @param enumClass The enum for the class
+     */
+    public EnumProvider(Class<T> enumClass) {
+        checkNotNull(enumClass, "enumClass");
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public boolean isProvided() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public T get(CommandArgs arguments, List<? extends Annotation> modifiers) throws ArgumentException, ProvisionException {
+        String name = arguments.next();
+        String test = simplify(name);
+
+        for (T entry : enumClass.getEnumConstants()) {
+            if (simplify(entry.name()).equalsIgnoreCase(test)) {
+                return entry;
+            }
+        }
+
+        throw new ArgumentParseException("No matching value found in the '" + enumClass.getSimpleName() + "' list.");
+    }
+
+    @Override
+    public List<String> getSuggestions(String prefix) {
+        return null;
+    }
+
+    private static String simplify(String t) {
+        return NON_ALPHANUMERIC.matcher(t.toLowerCase()).replaceAll("");
+    }
+}
